e151d256fc431903033d33af10521193e5ad4615
YuJHun <76084438+YuJHun@users.noreply.github.com>
2025-06-12 11:04:51 +0900

    추가 : 자동공격(임시) , 클릭공격-파티클이펙트 출력
    ClickManager.cs 생성
    AttackEffect.cs 생성
    AutoAttack.cs 생성
    클릭시 공격 UI클릭하면 공격안함
    텍스트매시프로

---
+++ Assets/01.Scripts/Player/AttackEffect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class AttackEffect : MonoBehaviour
+{
+    public ParticleSystem normalHitEffect;
+    public ParticleSystem criticalHitEffect;
+    public float criticalRate = 0.2f;
+
+    void OnEnable()
+    {
+        ClickManager.OnClick += HandleClick;
+    }
+
+    void OnDisable()
+    {
+        ClickManager.OnClick -= HandleClick;
+    }
+
+    void HandleClick(Vector3 position)
+    {
+        bool isCritical = Random.value < criticalRate;
+
+        ParticleSystem effectPrefab = isCritical ? criticalHitEffect : normalHitEffect;
+        ParticleSystem instance = Instantiate(effectPrefab, position, Quaternion.identity);
+        instance.Play(); // 명시적으로 재생 (필요 시)
+        Destroy(instance.gameObject, instance.main.duration); // 자동으로 꺼지도록 설정
+        Debug.Log(isCritical ? "치명타!" : "일반 공격");
+    }
+}
---
+++ Assets/01.Scripts/Player/AutoAttack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoAttack : MonoBehaviour
+{
+    public float attackInterval = 1.5f;//공격 간격
+    public bool isAutoAttackEnabled = true;//자동 공격 활성화 여부
+
+    void Start()
+    {
+        StartCoroutine(AutoAttackRoutine());
+    }
+
+    IEnumerator AutoAttackRoutine()
+    {
+        while (isAutoAttackEnabled)
+        {
+            Attack(); // 나중에 연결
+            yield return new WaitForSeconds(attackInterval);//attackInterval(초)만큼 잠시 기다림
+        }
+    }
+
+    void Attack()
+    {
+        Debug.Log("자동 공격!");
+    }
+}
