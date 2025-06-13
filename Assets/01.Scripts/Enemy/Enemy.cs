1c2e4549aae41c38fa3a801a016fa6c9922b7adb
OnionKimchi <jungjung21b@gmail.com>
2025-06-13 20:54:23 +0900

    Enemy.cs에 메서드 추가
    추가: TakeDamage(int amount)를 추가해둬서 플레이어 쪽에서 호출할 수 있게 만듬

--- Assets/01.Scripts/Enemy/Enemy.cs
+++ Assets/01.Scripts/Enemy/Enemy.cs
@@ -11,11 +11,28 @@ public class Enemy : MonoBehaviour
     public void Init(EnemyData enemyData, int stageIndex)
     {
         data = enemyData;
-        // 선형 증가: 10%씩 증가
+        // 선형 증가: 10%씩 증가로 임시 구현
         float healthMultiplier = 1f + stageIndex * 0.1f;
         float damageMultiplier = 1f + stageIndex * 0.1f;
 
         currentHealth = Mathf.RoundToInt(data.health * healthMultiplier);
         currentDamage = Mathf.RoundToInt(data.damage * damageMultiplier);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        //피격 애니메이션 구현 예정입니다.
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // 사망 처리 (이펙트, 점수, EnemyManager에 알리기,사망 애니메이션 등을 구현 예정입니다)
+
+        Destroy(gameObject);
     }
 }
